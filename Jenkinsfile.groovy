8c2269bb4ef6b2c6cde271ccc8ee579946eca86a
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-09 02:20:01 -0500

    test jenkinsfile without funcions

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,8 +5,27 @@ def call() {
         agent any
 
         stages {
-            buildStage()
-            analisysStage()
+            //buildStage()
+            //analisysStage()
+            stage('Checkout') {
+            steps {
+                git branch: 'feature', credentialsId: 'token_github', url: 'https://github.com/JulianMolina99/nodejs_application.git'
+            }
+        }
+
+        stage('Build app NodeJS') {
+            steps {
+                sh 'npm install'
+                sh 'npm run build'
+            }
+        }
+
+        stage('Build artifact') {
+            steps {
+                sh 'tar -czvf nodejs_app.tar.gz dist'
+                archiveArtifacts artifacts: 'nodejs_app.tar.gz', fingerprint: true
+            }
+        }
         }
     }
 }
