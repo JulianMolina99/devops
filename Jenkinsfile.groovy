1727791b96d387b3d0328fa36af8cfd672890a40
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-09 02:22:42 -0500

    test jenkinsfile with hello

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,6 @@
 @Library('NodeJSApp_Library@feature') _
 
+/*
 def call() {
     pipeline {
         agent any
@@ -7,27 +8,12 @@ def call() {
         stages {
             //buildStage()
             //analisysStage()
-            stage('Checkout') {
-            steps {
-                git branch: 'feature', credentialsId: 'token_github', url: 'https://github.com/JulianMolina99/nodejs_application.git'
-            }
-        }
-
-        stage('Build app NodeJS') {
-            steps {
-                sh 'npm install'
-                sh 'npm run build'
-            }
-        }
-
-        stage('Build artifact') {
-            steps {
-                sh 'tar -czvf nodejs_app.tar.gz dist'
-                archiveArtifacts artifacts: 'nodejs_app.tar.gz', fingerprint: true
-            }
-        }
+      
         }
     }
 }
+*/
+hello()
 
-return this
+
+//return this
