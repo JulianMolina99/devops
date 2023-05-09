797c225ee4708e089411881307adc3a93926af22
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-09 00:42:23 -0500

    return stages in buildStage function

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -1,21 +1,23 @@
-def call(){
-    stage ('Checkout') {
-        steps {
-            git credentialsId: 'token_github', url: 'https://github.com/JulianMolina99/nodejs_application.git'
+def buildStage() {
+    return {
+        stage('Checkout') {
+            steps {
+                git credentialsId: 'token_github', url: 'https://github.com/JulianMolina99/nodejs_application.git'
+            }
         }
-    }
 
-    stage('Build app NodeJS') {
-        steps {
-            sh 'npm install'
-            sh 'npm run build'
+        stage('Build app NodeJS') {
+            steps {
+                sh 'npm install'
+                sh 'npm run build'
+            }
         }
-    }
 
-    stage('Build artifact') {
-        steps {
-            sh 'tar -czvf nodejs_app.tar.gz dist'
-            archiveArtifacts artifacts: 'nodejs_app.tar.gz', fingerprint: true
+        stage('Build artifact') {
+            steps {
+                sh 'tar -czvf nodejs_app.tar.gz dist'
+                archiveArtifacts artifacts: 'nodejs_app.tar.gz', fingerprint: true
+            }
         }
     }
 }
