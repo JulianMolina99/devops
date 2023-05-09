c3cbb5f5d6fc9627a3d5db4a33bdfdc421381210
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-09 02:42:58 -0500

    Jenkinsfile with pipeline function

--- vars/buildStage.groovy
+++ vars/buildStage.groovy
@@ -1,8 +1,6 @@
 def call() {
-    return {
-        git branch: 'feature', credentialsId: 'token_github', url: 'https://github.com/JulianMolina99/nodejs_application.git'
-        sh 'npm install'
-        sh 'npm run build'
-    }
+    git branch: 'feature', credentialsId: 'token_github', url: 'https://github.com/JulianMolina99/nodejs_application.git'
+    sh 'npm install'
+    sh 'npm run build'
 }
 
