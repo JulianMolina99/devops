831e99c4770eb78b71192bc119be7e4472bf5e87
Julian Steven Molina Campos <110347693+JulianMolina99@users.noreply.github.com>
2023-05-15 21:42:44 -0500

    Update cloneRepository.groovy

--- vars/cloneRepository.groovy
+++ vars/cloneRepository.groovy
@@ -1,3 +1,3 @@
 def call(Map parameters){
-    git branch:'master', credentialsId: 'token_github', url: https://github.com/JulianMolina99/nodejs_application.git
+    git branch:'master', credentialsId: 'token_github', url: 'https://github.com/JulianMolina99/nodejs_application.git'
 }
