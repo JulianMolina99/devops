f162e0a113f3f879ddc70429cb488523a3ecabe1
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-16 23:28:27 -0500

    update owaspScan function

--- vars/owaspScan.groovy
+++ vars/owaspScan.groovy
@@ -1,12 +1,12 @@
 def call(){
     def myImage = docker.image('owasp/zap2docker-stable')
-    withEnv(["BUILD_ID=${env.BUILD_ID}"]) {
-        myImage.withRun('-u root --network=network_nodejs_app_${BUILD_ID} -v owasp_data:/zap/wrk/:rw') { c ->
+   
+    myImage.withRun('-u root --network=network_nodejs_app -v owasp_data:/zap/wrk/:rw') { c ->
         sh "docker exec ${c.id} zap-baseline.py -t http://nodejs_app:3000 -r report_baseInline.html"
     }
 
-    myImage.withRun('-u root --network=network_nodejs_app_${BUILD_ID} -v owasp_data:/zap/wrk/:rw') { c ->
+    myImage.withRun('-u root --network=network_nodejs_app -v owasp_data:/zap/wrk/:rw') { c ->
         sh "docker exec ${c.id} zap-full-scan.py.py -t http://nodejs_app:3000 -r report_fullScan.html"
     }
-    }
+    
 }
