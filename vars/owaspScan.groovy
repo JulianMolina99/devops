5176ba667402d3a60e3093ee43892f5860e8cd90
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-17 00:52:12 -0500

    update owaspScan

--- vars/owaspScan.groovy
+++ vars/owaspScan.groovy
@@ -1,15 +1,9 @@
 def call(){
-/*
-    def myImage = docker.image('owasp/zap2docker-stable')
-   
-    myImage.withRun("-u root --network=${env.JOB_NAME}_network_nodejs_app -v owasp_data:/zap/wrk/:rw") { c ->
-        sh "docker exec ${c.id} zap-baseline.py -t http://nodejs_app:3000 -r report_baseInline.html"
+
+    def owaspImage = docker.image('owasp/zap2docker-stable')
+    owaspImage.withRun("-u root --network=${env.JOB_NAME}_network_nodejs_app -v owasp_data:/zap/wrk/:rw") { c ->
+        sh "docker exec ${c.id} zap-baseline.py -t http://nodejs_app:3000 -r report_baseInline.html || true"
     }
 
-    myImage.withRun("-u root --network=${env.JOB_NAME}_network_nodejs_app -v owasp_data:/zap/wrk/:rw") { c ->
-        sh "docker exec ${c.id} zap-full-scan.py -t http://nodejs_app:3000 -r report_fullScan.html"
-    }
-    */
-
-    sh 'docker run -u root --network=reto_1_network_nodejs_app -v owasp_data:/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://nodejs_app:3000 -r report_baseInline.html'
+    //sh 'docker run -u root --network=${env.JOB_NAME}_network_nodejs_app -v owasp_data:/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://nodejs_app:3000 -r report_baseInline.html'
 }
