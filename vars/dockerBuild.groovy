7fd375f3bd750eade94ad5ad5cecb8392eb1a85e
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-17 00:05:09 -0500

    update functions

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,4 @@
 def call(){
- def dockerImage = docker.build("julianmol007/nodejs_app:${env.BUILD_ID}")
+    println("JOB_NAME: ${env.JOB_NAME}") 
+    def dockerImage = docker.build("julianmol007/nodejs_app:${env.BUILD_ID}")
 }
--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,5 +1,7 @@
 def call(){
+    
     withEnv(["COMPOSE_PROJECT_NAME=", "BUILD_ID=${env.BUILD_ID}"]) {
         sh 'docker-compose up -d'
     }
+    
 }
