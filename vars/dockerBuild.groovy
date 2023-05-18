6bf7331e949a4b8c7a642d2b2aa95c08128178f1
Julian Steven Molina Campos <110347693+JulianMolina99@users.noreply.github.com>
2023-05-18 17:56:53 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,5 @@
 def call(){
-    def projectName = build.environment.get("GIT_URL").replaceAll('https://github.com/', '').replaceAll('.git', '')
-    println(projectName)
+    println("Url project: ${env.GIT_URL}")
     println("JOB_NAME: ${env.JOB_NAME}")
     def dockerImage = docker.build("julianmol007/${env.JOB_NAME}_nodejs_app:${env.BUILD_ID}")
 }
