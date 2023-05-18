962962345bf2bef34c8b4358b95ace5fb82f2b8b
Julian Steven Molina Campos <110347693+JulianMolina99@users.noreply.github.com>
2023-05-18 16:59:26 -0500

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,4 +1,4 @@
 def call(){
     println("JOB_NAME: ${env.JOB_NAME}") 
-    def dockerImage = docker.build("julianmol007/nodejs_app:${env.BUILD_ID}")
+    def dockerImage = docker.build("julianmol007/${env.JOB_NAME}_nodejs_app:${env.BUILD_ID}")
 }
