accc725b6b106e3ed43e31f2e4aa7e2198c86f0e
JulianMolina99 <julianmolinac99@gmail.com>
2023-05-22 15:04:39 -0500

    update analysys sonar function

--- vars/owaspScan.groovy
+++ vars/owaspScan.groovy
@@ -11,5 +11,3 @@ def call() {
 }
 
 
-
-//sh 'docker run -u root --network=${env.JOB_NAME}_network_nodejs_app -v owasp_data:/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://nodejs_app:3000 -r report_baseInline.html'
